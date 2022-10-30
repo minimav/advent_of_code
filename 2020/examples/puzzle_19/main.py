6d05eda095772e272e7c89e2878cb2bb057908c1
minimav <6566948+minimav@users.noreply.github.com>
2022-10-30 19:57:43 +0000

    Puzzle 19 part 2 2020 solution (Python)

--- 2020/examples/puzzle_19/main.py
+++ 2020/examples/puzzle_19/main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Set, Tuple
 
 
 def parse_file(file_name: str = "input.txt") -> dict:
@@ -15,14 +15,14 @@ def parse_file(file_name: str = "input.txt") -> dict:
                 if "|" in raw_rule:
                     raw_first_rule, raw_second_rule, *_ = raw_rule.split("|")
                     rules[rule_number] = (
-                        raw_first_rule.split(),
-                        raw_second_rule.split(),
+                        tuple(raw_first_rule.split()),
+                        tuple(raw_second_rule.split()),
                     )
                 elif '"' in line:
                     # looks like '"a"' or '"b"', extract character
                     rules[rule_number] = raw_rule[1]
                 else:
-                    rules[rule_number] = raw_rule.split()
+                    rules[rule_number] = tuple(raw_rule.split())
             elif not line:
                 continue
             else:
@@ -30,68 +30,74 @@ def parse_file(file_name: str = "input.txt") -> dict:
     return {"rules": rules, "messages": messages}
 
 
-def evaluate_message(message: str, rules: dict) -> bool:
-    """Evaluate rules on a message."""
+def find_valid_messages(rules, messages: Set[str], verbose: bool = False) -> Set[str]:
+    """Iteratively find valid messages."""
+    answers = set()
+    max_len = max(len(m) for m in messages)
 
-    def evaluate(messages: List[str], rule) -> List[str]:
-        """Recursive evaluation of the rules on a message."""
-        valid_messages = [message for message in messages if message]
-        if not valid_messages:
-            # still something to evaluate but message has been read
-            return []
-
-        next_messages = []
-        if isinstance(rule, str):
-            for message in valid_messages:
-                if message[0] != rule:
+    def worker(possible_messages: Set[tuple], iteration=0):
+        if verbose:
+            print(f"{iteration=} {len(possible_messages)} to process")
+        next_possible_messages = set()
+        for partial_message, rule_numbers in possible_messages:
+            rule_number, *remaining_rule_numbers = rule_numbers
+            remaining_rule_numbers = tuple(remaining_rule_numbers)
+            rule = rules[rule_number]
+            if isinstance(rule, str):
+                new_partial_message = partial_message + rule
+                if len(new_partial_message) > max_len:
                     continue
-                if len(message) == 1:
-                    raise ValueError
-                else:
-                    next_messages.append(message[1:])
+                elif new_partial_message in messages:
+                    if not remaining_rule_numbers:
+                        answers.add(new_partial_message)
+                    else:
+                        next_possible_messages.add(
+                            (new_partial_message, remaining_rule_numbers)
+                        )
+                elif not remaining_rule_numbers:
+                    continue
+                elif any(m.startswith(new_partial_message) for m in messages):
+                    # only keep this if at least one message starts in this way
+                    # need this to keep the possible messages list small enough
+                    next_possible_messages.add(
+                        (new_partial_message, remaining_rule_numbers)
+                    )
+            elif isinstance(rule[0], tuple):
+                next_possible_messages.add(
+                    (partial_message, rule[0] + remaining_rule_numbers)
+                )
+                next_possible_messages.add(
+                    (partial_message, rule[1] + remaining_rule_numbers)
+                )
+            else:
+                next_possible_messages.add(
+                    (partial_message, rule + remaining_rule_numbers)
+                )
 
-        elif isinstance(rule, tuple):
-            next_messages += evaluate(messages, rule[0])
-            next_messages += evaluate(messages, rule[1])
-        else:
-            for rule_number in rule:
-                messages = evaluate(messages, rules[rule_number])
-                if not messages:
-                    break
+        if not next_possible_messages:
+            return
+        return worker(possible_messages=next_possible_messages, iteration=iteration + 1)
 
-            next_messages = messages
-        return next_messages
-
-    try:
-        evaluate([message], rules["0"])
-        return False
-    except ValueError:
-        return True
+    worker(possible_messages=[("", rules["0"])], iteration=0)
+    return answers
 
 
 def part_1(data: dict) -> int:
-    answer = 0
-    for message in data["messages"]:
-        if evaluate_message(message, data["rules"]):
-            answer += 1
-    return answer
+    valid_messages = find_valid_messages(
+        rules=data["rules"], messages=set(data["messages"])
+    )
+    return len(valid_messages)
 
 
 def part_2(data: dict) -> int:
-    # 8: 42 | 42 8
-    # 11: 42 31 | 42 11 31
-    data["rules"]["8"] = (["42"], ["42", "8"])
-    data["rules"]["11"] = (["42", "31"], ["42", "11", "31"])
-    answer = 0
-    for message in data["messages"]:
-        if evaluate_message(message, data["rules"]):
-            answer += 1
-    return answer
+    data["rules"]["8"] = (("42",), ("42", "8"))
+    data["rules"]["11"] = (("42", "31"), ("42", "11", "31"))
+    return part_1(data)
 
 
 if __name__ == "__main__":
     data = parse_file(file_name="input.txt")
     answer = part_1(data)
-    print(f"Part 1 {answer=}")
+    print(f"Part 1: {answer=}")
     answer = part_2(data)
-    print(f"Part 2 {answer=}")
+    print(f"Part 2: {answer=}")
