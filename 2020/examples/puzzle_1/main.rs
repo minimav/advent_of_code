d28ccb1d5da6a52b3b2a6d26648f1771326c18ea
minimav <6566948+minimav@users.noreply.github.com>
2022-12-28 09:53:51 +0000

    Puzzle 1 2020 tidy up

--- 2020/examples/puzzle_1/main.rs
+++ 2020/examples/puzzle_1/main.rs
@@ -1,75 +1,45 @@
-use std::collections::HashSet;
-use std::fs;
+use itertools::Itertools;
 use std::time::Instant;
 
-/* Product of the pair that sums to the target (part 1). */
-fn find_product(filename: String, target: i32) -> Result<i32, String> {
-    let contents = fs::read_to_string(filename).expect("Could not read file");
-
-    let mut values = HashSet::<i32>::new();
-    for raw_value in contents.lines() {
-        let value: i32 = raw_value.trim().parse().unwrap();
-
-        if values.contains(&(target - value)) {
-            println!(
-                "{} * {} = {}",
-                target - value,
-                value,
-                (target - value) * value
-            );
-            return Ok((target - value) * value);
-        } else {
-            values.insert(value);
-        }
-    }
-    return Err(format!("No values found which sum to {}", target));
+fn find_product(contents: &str, target: u32, combination_size: usize) -> Option<u32> {
+    contents
+        .lines()
+        .map(|x| x.parse::<u32>().unwrap())
+        .combinations(combination_size)
+        .filter(|x| x.iter().sum::<u32>() == target)
+        .map(|x| x.into_iter().product())
+        .next()
 }
 
-/* Product of the triple that sums to the target (part 2). */
-fn find_triple_product(filename: String, target: i32) -> Result<i32, String> {
-    let contents = fs::read_to_string(filename).expect("Could not read file");
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
 
-    let mut _groups = HashSet::<Vec<i32>>::new();
-    for raw_value in contents.lines() {
-        let mut new_groups = HashSet::<Vec<i32>>::new();
-        let value: i32 = raw_value.trim().parse().unwrap();
-        for group in _groups.iter() {
-            let mut new_group = group.to_vec().clone();
-            new_group.push(value);
-
-            if new_group.len() == 3 {
-                if new_group.iter().sum::<i32>() == target {
-                    let answer = new_group.iter().product::<i32>();
-                    println!("{:?} product is {}", new_group, answer);
-                    return Ok(answer);
-                }
-            } else {
-                new_groups.insert(new_group.to_vec());
-            }
-        }
-        let initial_group = vec![value];
-        new_groups.insert(initial_group);
-        for group in new_groups.iter() {
-            _groups.insert(group.to_vec());
-        }
+    #[test]
+    fn test_part_1_example() {
+        assert_eq!(
+            find_product(include_str!("./example.txt"), 2020, 2),
+            Some(514579)
+        );
     }
-    return Err(format!("No values found which sum to {}", target));
+
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(
+            find_product(include_str!("./example.txt"), 2020, 3),
+            Some(241861950)
+        );
+    }
 }
 
 fn main() {
     let start = Instant::now();
-    let filenames: Vec<String> = vec![
-        "examples/puzzle_1/example_input.txt".to_string(),
-        "examples/puzzle_1/input.txt".to_string(),
-    ];
-    let target: i32 = 2020;
-    for filename in filenames {
-        find_product(filename, target).expect("Input has no solution");
-    }
-
-    find_triple_product("examples/puzzle_1/input.txt".to_string(), target)
-        .expect("Input has no solution");
-
+    let contents = include_str!("./input.txt");
+    let part_1_answer = find_product(contents, 2020, 2);
+    println!("Answer for part 1 is: {:?}", part_1_answer);
+    let part_2_answer = find_product(contents, 2020, 3);
+    println!("Answer for part 2 is: {:?}", part_2_answer);
     let duration = start.elapsed();
     println!("Took {:?} to solve this puzzle", duration);
 }
