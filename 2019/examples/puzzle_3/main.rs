0b08ce9981f91651af511f246916caeaf4229522
minimav <6566948+minimav@users.noreply.github.com>
2023-02-15 18:15:01 +0000

    Puzzle 3 2019 part 2

--- 2019/examples/puzzle_3/main.rs
+++ 2019/examples/puzzle_3/main.rs
@@ -5,6 +5,7 @@ struct VerticalLine {
     x: i64,
     y1: i64,
     y2: i64,
+    steps: u64,
 }
 
 #[derive(Debug)]
@@ -12,17 +13,29 @@ struct HorizontalLine {
     y: i64,
     x1: i64,
     x2: i64,
+    steps: u64,
 }
 
 #[derive(Debug)]
 struct Point {
     x: i64,
     y: i64,
+    steps: u64,
 }
 
 impl Point {
     fn manhattan_distance(&self) -> i64 {
         self.x.abs() + self.y.abs()
+    }
+
+    fn new(x: i64, y: i64, steps: u64) -> Self {
+        Point { x, y, steps }
+    }
+}
+
+impl Default for Point {
+    fn default() -> Self {
+        Point::new(0, 0, 0)
     }
 }
 
@@ -33,9 +46,13 @@ impl VerticalLine {
         let horizontal_hit = (hline.x1 <= self.x && hline.x2 >= self.x)
             || (hline.x1 >= self.x && hline.x2 <= self.x);
         if vertical_hit && horizontal_hit {
+            // lines counts steps till their end, so we need to subtract the
+            // portion of them that is not traversed, i.e. after the intersection
             Some(Point {
                 x: self.x,
                 y: hline.y,
+                steps: self.steps - self.y2.abs_diff(hline.y) + hline.steps
+                    - hline.x2.abs_diff(self.x),
             })
         } else {
             None
@@ -47,7 +64,7 @@ fn parse_lines(instructions: &str) -> (Vec<VerticalLine>, Vec<HorizontalLine>) {
     let mut vertical_lines: Vec<VerticalLine> = vec![];
     let mut horizontal_lines: Vec<HorizontalLine> = vec![];
 
-    let mut current_point = Point { x: 0, y: 0 };
+    let mut current_point = Point::default();
     for command in instructions.split(",") {
         let steps = command[1..].parse::<i64>().unwrap();
         match command.chars().nth(0) {
@@ -56,11 +73,13 @@ fn parse_lines(instructions: &str) -> (Vec<VerticalLine>, Vec<HorizontalLine>) {
                     x: current_point.x,
                     y1: current_point.y,
                     y2: current_point.y + steps,
+                    steps: current_point.steps + steps as u64,
                 };
                 vertical_lines.push(vline);
                 current_point = Point {
                     x: current_point.x,
                     y: current_point.y + steps,
+                    steps: current_point.steps + steps as u64,
                 };
             }
             Some('D') => {
@@ -68,11 +87,13 @@ fn parse_lines(instructions: &str) -> (Vec<VerticalLine>, Vec<HorizontalLine>) {
                     x: current_point.x,
                     y1: current_point.y,
                     y2: current_point.y - steps,
+                    steps: current_point.steps + steps as u64,
                 };
                 vertical_lines.push(vline);
                 current_point = Point {
                     x: current_point.x,
                     y: current_point.y - steps,
+                    steps: current_point.steps + steps as u64,
                 };
             }
             Some('L') => {
@@ -80,11 +101,13 @@ fn parse_lines(instructions: &str) -> (Vec<VerticalLine>, Vec<HorizontalLine>) {
                     y: current_point.y,
                     x1: current_point.x,
                     x2: current_point.x - steps,
+                    steps: current_point.steps + steps as u64,
                 };
                 horizontal_lines.push(hline);
                 current_point = Point {
                     x: current_point.x - steps,
                     y: current_point.y,
+                    steps: current_point.steps + steps as u64,
                 };
             }
             Some('R') => {
@@ -92,11 +115,13 @@ fn parse_lines(instructions: &str) -> (Vec<VerticalLine>, Vec<HorizontalLine>) {
                     y: current_point.y,
                     x1: current_point.x,
                     x2: current_point.x + steps,
+                    steps: current_point.steps + steps as u64,
                 };
                 horizontal_lines.push(hline);
                 current_point = Point {
                     x: current_point.x + steps,
                     y: current_point.y,
+                    steps: current_point.steps + steps as u64,
                 };
             }
             _ => {
@@ -146,7 +171,38 @@ fn part_1(contents: &str) -> i64 {
 }
 
 fn part_2(contents: &str) -> u64 {
-    0
+    let instructions: Vec<&str> = contents.split("\n").collect();
+    let wire_1 = instructions[0];
+    let wire_2 = instructions[1];
+    let (wire_1_vertical, wire_1_horizontal) = parse_lines(wire_1);
+    let (wire_2_vertical, wire_2_horizontal) = parse_lines(wire_2);
+
+    let mut minimal_steps = u64::MAX;
+    for vline in wire_1_vertical.iter() {
+        for hline in wire_2_horizontal.iter() {
+            match vline.intersection(hline) {
+                Some(p) => {
+                    if p.steps > 0 && p.steps < minimal_steps {
+                        minimal_steps = p.steps;
+                    }
+                }
+                None => {}
+            }
+        }
+    }
+    for vline in wire_2_vertical.iter() {
+        for hline in wire_1_horizontal.iter() {
+            match vline.intersection(hline) {
+                Some(p) => {
+                    if p.steps > 0 && p.steps < minimal_steps {
+                        minimal_steps = p.steps;
+                    }
+                }
+                None => {}
+            }
+        }
+    }
+    minimal_steps
 }
 
 #[cfg(test)]
@@ -164,8 +220,22 @@ mod tests {
         "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
         135
     )]
-    fn test_(#[case] input: &str, #[case] expected: i64) {
+    fn test_part_1_examples(#[case] input: &str, #[case] expected: i64) {
         assert_eq!(part_1(input), expected);
+    }
+
+    #[rstest]
+    #[case("R8,U5,L5,D3\nU7,R6,D4,L4", 30)]
+    #[case(
+        "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83",
+        610
+    )]
+    #[case(
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        410
+    )]
+    fn test_part_2_examples(#[case] input: &str, #[case] expected: u64) {
+        assert_eq!(part_2(input), expected);
     }
 }
 
