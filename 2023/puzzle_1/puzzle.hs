e1e7e4e1ddcf169540c60beafd892a65ccc1d753
minimav <6566948+minimav@users.noreply.github.com>
2023-12-01 20:56:43 +0000

    Puzzle 1 2023 in Haskell

--- 2023/puzzle_1/puzzle.hs
+++ 2023/puzzle_1/puzzle.hs
@@ -1,17 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 import System.IO
-import Data.Char (isDigit, digitToInt, isLetter)
-import Data.Text(pack, unpack, replace)
-import Data.List(findIndex, tails, isPrefixOf)
-import Data.Maybe (catMaybes)
-import Data.List (isInfixOf, find)
+import Data.Char (digitToInt, isLetter)
+import Data.List(tails, isPrefixOf)
 
-
+notLetter :: Char -> Bool
 notLetter = not . isLetter
+
+filterDigitChars :: [Char] -> [Char]
 filterDigitChars xs = filter notLetter xs
+
+toDigits :: [Char] -> [Int]
 toDigits xs = map digitToInt xs
+
+fromDigits :: [Int] -> Int
 fromDigits :: [Int] -> Int = foldl addDigit 0
    where addDigit num d = 10 * num + d
+
+topAndTail :: [a] -> [a]
 topAndTail xs = [head xs, last xs]
 
 solvePart1 path = do  
@@ -24,61 +29,79 @@ solvePart1 path = do
         let answer = sum rowNumbers
         print answer
 
+numberStringWithDigit :: [([Char], Int)]
+numberStringWithDigit = [("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
 
-numberStringToDigit = [("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")]
-numberStrings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+-- Determine whether the character at each index in a string is not a letter
+indexWithNotALetter :: [Char] -> [(Int, Bool)]
+indexWithNotALetter xs = map (\(x, y) -> (x, notLetter y)) (zip [0..] xs)
 
-findString :: (Eq a) => [a] -> [a] -> Maybe Int
-findString search str = findIndex (isPrefixOf search) (tails str)
-finders = map findString numberStrings
+-- Filter to indexes where the value is not a letter, i.e. a number
+whereNotALetter :: [(Int, Bool)] -> [(Int, Bool)]
+whereNotALetter xs = filter (\(x, y) -> y == True) xs
 
---for a list of tuples, find the first element of the tuple whose second element is the minimum
--- minTuple :: (Ord b) => [(a, b)] -> a
--- minTuple xs = fst $ foldl1 (\acc x -> if snd x < snd acc then x else acc) xs
+-- Get the int digit at a given index in a string
+intAtIndex :: Int -> [Char] -> (Int, Int)
+intAtIndex index xs = (index, digitToInt (xs !! index))
 
---applyFuns should take a list of tuples, where the first element of each tuple is a key, and the second element is a list of functions
---it should then apply each function to the input, and return a list of tuples where the first element is the key, and the second element is the result of applying the function
--- applyFuns :: [(c, a->b)] -> a -> [(c, b)]
--- applyFuns xs input = map (\(x, y) -> (x, y input)) xs
+-- Get the int digit at the given index in a string for multiple indexes
+getIntAtIndex :: ([(Int, Bool)], [Char]) -> [(Int, Int)]
+getIntAtIndex (indexes, xs) = map (\(x, y) -> intAtIndex x xs) indexes
 
--- applyToSecond :: (a -> b) -> [(c, a)] -> [(c, b)]
--- applyToSecond xs func = map (\(x, y) -> (x, func y)) xs
+-- Get the int digit at the min index
+intAtMinIndex :: [(Int, Int)] -> Int
+intAtMinIndex xs = snd (head (filter (\(x, y) -> x == minIndex) xs))
+    where minIndex = minimum (map fst xs)
 
+-- Get the int digit at the max index
+intAtMaxIndex :: [(Int, Int)] -> Int
+intAtMaxIndex xs = snd (head (filter (\(x, y) -> x == maxIndex) xs))
+    where maxIndex = maximum (map fst xs)
 
---replace a substring at a particular index
-replaceSubstringAtIndex :: Int -> String -> String -> String
-replaceSubstringAtIndex start replacement str = (take start str) ++ replacement ++ (drop (start + (length replacement)) str)
+-- Concatenate two lists of tuples
+concatTuple :: ([(Int, Int)], [(Int, Int)]) -> [(Int, Int)]
+concatTuple (xs, ys) = xs ++ ys
 
---for a string s, find the first substring in s that matches an element of numberStrings
---replace the whole substring with the corresponding digit then return the result
---if no substring matches, return the original string
---replaceNumberStrings :: String -> String
---test cases
---replaceNumberStrings "eightwothree" == "8twothree"
---replaceNumberStrings "ash" == "ash"
---replaceNumberStrings "onetwothree" == "1twothree"
---replaceNumberStrings "1two3" == "123"
---replaceNumberStrings should compile and the above test cases should all hold
+-- Convert a pair to a list
+pairToList :: (a, a) -> [a]
+pairToList (x,y) = [x,y]
+
+-- Cartesian product of two lists
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+-- (("one","1"),(0,"eighthree")) check 
+filterSubstrings :: [(([Char], Int), (Int, [Char]))] -> [(([Char], Int), (Int, [Char]))]
+filterSubstrings xs = filter (\(x, y) -> isPrefixOf (fst x) (snd y)) xs
+
+-- (("one","1"),(0,"eighthree")) extract index and digit corresponding to substring
+extractDigitSubstring :: (([Char], Int), (Int, [Char])) -> (Int, Int)
+extractDigitSubstring ((x, y), (z, w)) = (z, y)
 
 
 solvePart2 path = do  
         content <- readFile path
         let linesOfFile = lines $ content
-        print linesOfFile
-        let cleanedLinesOfFile = map replaceFirstNumberStrings linesOfFile
-        print cleanedLinesOfFile
-        let digitChars = map filterDigitChars cleanedLinesOfFile
-        let allDigits = map toDigits digitChars
-        print allDigits
-        let digits = map topAndTail allDigits
-        let rowNumbers = map fromDigits digits
-        let answer = sum rowNumbers
+        
+        let indexesNotALetter = map indexWithNotALetter linesOfFile
+        -- Get the indexes of the digits in each line
+        let indexesWithDigits = map getIntAtIndex (zip (map whereNotALetter indexesNotALetter) linesOfFile)
+
+        -- Get the same format except the start index of number substrings
+        let tailsWithIndex = map (zip [0..] . tails) linesOfFile
+        let allTailsWithSubstrings = map (cartProd numberStringWithDigit) tailsWithIndex
+        let substringMatches = map filterSubstrings allTailsWithSubstrings
+        let indexesWithStringDigits = map (map extractDigitSubstring) substringMatches
+        
+        -- Combine both sets of indexes per line, then get the digit for the first and last indexes
+        let finalIndexesWithDigits = map concatTuple (zip indexesWithDigits indexesWithStringDigits)
+        let firstAndLast = zip (map intAtMinIndex finalIndexesWithDigits) (map intAtMaxIndex finalIndexesWithDigits)
+        let answer = sum (map fromDigits (map pairToList firstAndLast))
+
         print answer
 
 main = do
-    let s = replaceFirstNumberStrings "none1eight"
-    print s
-    --solvePart1 "./example_1_1.txt"
-    --solvePart1 "./input_1.txt"
-    solvePart2 "./example_1_2.txt"
-    --solvePart2 "./input_1.txt"
+    solvePart1 "puzzle_1/example_1_1.txt"
+    solvePart1 "puzzle_1/input_1.txt"
+    solvePart2 "puzzle_1/example_1_2.txt"
+    solvePart2 "puzzle_1/input_1.txt"
