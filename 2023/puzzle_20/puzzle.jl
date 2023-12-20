b949b4f022a7b05c32c1f15abd74b52aaf2c7c61
minimav <6566948+minimav@users.noreply.github.com>
2023-12-20 22:39:50 +0000

    Puzzle 20 part 1 2023 in Julia

--- 2023/puzzle_20/puzzle.jl
+++ 2023/puzzle_20/puzzle.jl
@@ -1,12 +1,190 @@
 
-function puzzle(input)
-    println(input)
+mutable struct Untyped
+    name::String
+    outputs::Vector{String}
+end
+
+function receive(un::Untyped, from::String, pulse::String)
+    return nothing
+end
+
+mutable struct FlipFlop
+    name::String
+    on::Bool
+    outputs::Vector{String}
+end
+
+function receive(ff::FlipFlop, from::String, pulse::String)
+    if pulse == "low"
+        ff.on = !ff.on
+        if ff.on
+            # Off to on case
+            return [(ff.name, "high", output) for output in ff.outputs]
+        else
+            # On to off case
+            return [(ff.name, "low", output) for output in ff.outputs]
+        end
+    end
+end
+
+mutable struct Conjunction
+    name::String
+    input_states::Dict{String, String}
+    outputs::Vector{String}
+
+    Conjunction(name, keys) = new(name, Dict(), keys)
+end
+
+function receive(c::Conjunction, from::String, pulse::String)
+    c.input_states[from] = pulse
+    if all(value == "high" for value in values(c.input_states))
+        return [(c.name, "low", output) for output in c.outputs]
+    else
+        return [(c.name, "high", output) for output in c.outputs]
+    end
+end
+
+mutable struct Broadcast
+    name::String
+    outputs::Vector{String}
+end
+
+function receive(b::Broadcast, from::String, pulse::String)
+    return [(b.name, pulse, output) for output in b.outputs]
+end
+
+
+function part_1(input)
+    modules = Dict{String, Any}()
+    conjunctions = Vector{String}()
+    for line in split(input, "\n")
+        rawKey, raw_outputs = split(line, " -> ")
+        outputs = [String(s) for s in split(raw_outputs, ", ")]
+        if startswith(rawKey, "%")
+            key = String(rawKey[2:end])
+            modules[key] = FlipFlop(key, false, outputs)
+        elseif startswith(rawKey, "&")
+            key = String(rawKey[2:end])
+            modules[key] = Conjunction(key, outputs)
+            conjunctions = vcat(conjunctions, key)
+        elseif startswith(rawKey, "broadcaster")
+            modules["broadcaster"] = Broadcast("broadcaster", outputs)
+        else
+            # Untyped case
+            modules[key] = Untyped(rawKey, [])
+        end
+    end
+
+     # Make sure all conjunction inputs are known
+    for (key, mod) in pairs(modules)
+        for output in mod.outputs
+            if output in conjunctions
+                modules[output].input_states[key] = "low"
+            end
+        end
+    end
+   
+    #println(modules)
+    
+    num_signals = Dict{String, Int}("low" => 0, "high" => 0)
+    for i in 1:1000
+        #println("########### Round $i")
+        # Start new round with same first signal
+        signals = [("button", "low", "broadcaster")]
+        while length(signals) > 0
+            signal = popfirst!(signals)
+            #println(signal)
+            from_key, pulse, to_key = signal
+            num_signals[pulse] += 1
+            if !(to_key in keys(modules))
+                modules[to_key] = Untyped(to_key, [])
+            end
+            
+            new_signals = receive(modules[to_key], from_key, pulse)
+            if new_signals != nothing
+                # Deal with no signals from one flip flop case
+                signals = vcat(signals, new_signals)
+            end
+        end
+    end
+
+    println(num_signals["low"] * num_signals["high"])
+end
+
+function part_2(input)
+    modules = Dict{String, Any}()
+    conjunctions = Vector{String}()
+    for line in split(input, "\n")
+        rawKey, raw_outputs = split(line, " -> ")
+        outputs = [String(s) for s in split(raw_outputs, ", ")]
+        if startswith(rawKey, "%")
+            key = String(rawKey[2:end])
+            modules[key] = FlipFlop(key, false, outputs)
+        elseif startswith(rawKey, "&")
+            key = String(rawKey[2:end])
+            modules[key] = Conjunction(key, outputs)
+            conjunctions = vcat(conjunctions, key)
+        elseif startswith(rawKey, "broadcaster")
+            modules["broadcaster"] = Broadcast("broadcaster", outputs)
+        else
+            # Untyped case
+            modules[key] = Untyped(rawKey, [])
+        end
+    end
+
+     # Make sure all conjunction inputs are known
+    for (key, mod) in pairs(modules)
+        for output in mod.outputs
+            if output in conjunctions
+                modules[output].input_states[key] = "low"
+            end
+        end
+    end
+   
+    num_presses = 0
+    while (num_presses < 10)
+        num_presses += 1
+   
+        # Start new round with same first signal
+        signals = [("button", "low", "broadcaster")]
+        while length(signals) > 0
+            signal = popfirst!(signals)
+            #println(signal)
+            from_key, pulse, to_key = signal
+            if (to_key == "rx") && (pulse == "low")
+                println(num_presses)
+                return
+            end
+
+            if !(to_key in keys(modules))
+                modules[to_key] = Untyped(to_key, [])
+            end
+            
+            new_signals = receive(modules[to_key], from_key, pulse)
+            if new_signals != nothing
+                # Deal with no signals from one flip flop case
+                signals = vcat(signals, new_signals)
+            end
+        end
+
+        println("########### Round $num_presses")
+        for mod in values(modules)
+            if mod isa FlipFlop
+                println(mod.name, " ", mod.on)
+            end
+        end
+    end
 end
 
 open("puzzle_20/example.txt") do example
-    puzzle(read(example, String))
+    part_1(read(example, String))
+end
+
+open("puzzle_20/example_2.txt") do example
+    part_1(read(example, String))
 end
 
 open("puzzle_20/input.txt") do input
-    puzzle(read(input, String))
+    #part_1(read(input, String))
+    part_2(read(input, String))
 end
