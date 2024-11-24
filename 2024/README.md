ed2ee4f8b6b6b4876de8a73d9f53bd3cd285ee09
minimav <6566948+minimav@users.noreply.github.com>
2024-11-24 08:16:40 +0000

    Setup 2024

---
+++ 2024/README.md
@@ -0,0 +1,5 @@
+Go is 2024's language of choice. Run solutions with:
+
+```bash
+go run puzzle_<id>/puzzle.go
+```
---
+++ 2024/puzzle_1/puzzle.go
@@ -0,0 +1,8 @@
+// go run puzzle_1/puzzle.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
