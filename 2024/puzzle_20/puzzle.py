f5b1f8fc42e1354583d09f5976a845bb1c1b0015
minimav <6566948+minimav@users.noreply.github.com>
2024-12-20 20:50:25 +0000

    Puzzle 20 part 2 2024

--- 2024/puzzle_20/puzzle.py
+++ 2024/puzzle_20/puzzle.py
@@ -1,5 +1,5 @@
 import heapq
-from collections import defaultdict
+from collections import Counter
 
 
 def print_grid(non_boundary_walls, boundary, start, end):
@@ -28,66 +28,79 @@ def solve(
     end: complex,
     skip_length: int = 0,
 ) -> int:
+    num_rows = max(int(coord.imag) for coord in boundary) + 1
+    num_columns = max(int(coord.real) for coord in boundary) + 1
+
     counter = 0
     results = {}
     current_best = {}
-    queue = [(0, counter, start, 0, [None, None])]
+    # Include has cheated status to force the no-cheat path to be explored first
+    queue = [(False, 0, counter, start, None)]
     while queue:
-        cost, *_, loc, cheat_cost, cheat_history = heapq.heappop(queue)
+        has_cheated, cost, *_, loc, cheat = heapq.heappop(queue)
         if loc == end:
-            # Cheat is uniquely defined by start and end - need length?
-            cheat_key = (cheat_history[0], cheat_history[1])
-            results[cheat_key] = cost
+            results[cheat] = cost
             continue
 
+        # Having cheated there is only one route to the finish
+        if has_cheated:
+            remainder = results[None] - current_best[(loc, None)]
+            final_cost = remainder + cost
+            if cheat in results and results[cheat] < final_cost:
+                continue
+            results[cheat] = final_cost
+            continue
+
+        # No cheat moves
         for move in (complex(0, 1), complex(1, 0), complex(-1, 0), complex(0, -1)):
             new_loc = loc + move
             if new_loc in boundary:
                 # Invalid move case
                 continue
+            elif new_loc in non_boundary_walls:
+                continue
 
-            new_cheat_history = [None, None]
-            new_cheat_cost = cheat_cost
-
-            if new_loc not in non_boundary_walls:
-                # No need to cheat case
-                if new_loc == cheat_history[0]:
-                    # Prevent going back to start of the cheat
-                    continue
-                if loc == cheat_history[1]:
-                    # Record correct end location of the cheat
-                    # Already during the cheat that the transition keeps us
-                    # under the skip length
-                    new_cheat_history = [cheat_history[0], new_loc]
-            else:
-                # Need to cheat case
-                if skip_length == 0:
-                    # Cheating not allowed for this param
-                    continue
-                if cheat_cost >= skip_length - 1:
-                    # Another step into a wall would be mean we'd need a further
-                    # step to get out of the wall
-                    # print("Can't continue cheating")
-                    continue
-                elif cheat_cost == 0:
-                    # Starting a cheat
-                    new_cheat_history = [loc, new_loc]
-                    # print("Starting a cheat")
-                else:
-                    # Continuing a cheat
-                    new_cheat_history = [cheat_history[0], new_loc]
-
-                new_cheat_cost += 1
-
-            key = (new_loc, tuple(new_cheat_history))
-            if key in current_best and current_best[key] < cost + 1:
+            key = (new_loc, None)
+            if key in current_best and current_best[key] < cost:
                 continue
 
             counter += 1
             current_best[key] = cost + 1
-            item = (cost + 1, counter, new_loc, new_cheat_cost, new_cheat_history)
-            # print(f"Queueing {item}")
+            item = (False, cost + 1, counter, new_loc, None)
             heapq.heappush(queue, item)
+
+        # Cheat moves
+        # Can teleport to any square within skip_length Manhattan distance
+        for total in range(2, skip_length + 1):
+            for horiz in range(total + 1):
+                vert = total - horiz
+                for move in (
+                    complex(horiz, vert),
+                    complex(-horiz, vert),
+                    complex(horiz, -vert),
+                    complex(-horiz, -vert),
+                ):
+                    new_loc = loc + move
+                    if (not (0 < new_loc.real < num_columns - 1)) or (
+                        not (0 < new_loc.imag < num_rows - 1)
+                    ):
+                        # Invalid move case
+                        continue
+                    elif new_loc in non_boundary_walls:
+                        continue
+
+                    cheat = (loc, new_loc)
+                    key = (new_loc, cheat)
+                    new_cost = cost + total
+                    if key in current_best and current_best[key] < new_cost:
+                        continue
+
+                    counter += 1
+                    current_best[key] = new_cost
+                    item = (True, new_cost, counter, new_loc, cheat)
+                    # print(f"Queueing {item}")
+                    heapq.heappush(queue, item)
+
     return results
 
 
@@ -114,10 +127,8 @@ def find_cheats(input: str, skip_length: int, min_saving: int):
             elif char == "E":
                 end = coord
 
-    print_grid(non_boundary_walls, boundary, start, end)
-    best_cost = solve(non_boundary_walls, boundary, start, end, skip_length=0)[
-        None, None
-    ]
+    # print_grid(non_boundary_walls, boundary, start, end)
+    best_cost = solve(non_boundary_walls, boundary, start, end, skip_length=0)[None]
     print(f"Best cost: {best_cost}")
 
     costs_with_skips = solve(
@@ -126,21 +137,18 @@ def find_cheats(input: str, skip_length: int, min_saving: int):
 
     savings = {k: best_cost - v for k, v in costs_with_skips.items() if best_cost > v}
     print(f"Number cheats: {len(savings)}")
-    answer = sum(v for v in costs_with_skips.values() if v >= min_saving)
+    # print(sorted(Counter(v for v in savings.values() if v >= min_saving).items()))
+    answer = sum(1 for v in savings.values() if v >= min_saving)
     print(f"Answer: {answer}")
-
-
-def part_2(input: str):
-    pass
 
 
 if __name__ == "__main__":
     with open("puzzle_20/example.txt", "r") as f:
         example = f.read()
         find_cheats(example, skip_length=2, min_saving=100)
-        # find_cheats(example, skip_length=20, min_saving=50)
+        find_cheats(example, skip_length=20, min_saving=50)
 
     with open("puzzle_20/input.txt", "r") as f:
         input = f.read()
         find_cheats(input, skip_length=2, min_saving=100)
-        # find_cheats(input, skip_length=20, min_saving=100)
+        find_cheats(input, skip_length=20, min_saving=100)
